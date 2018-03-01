f4f36af28acf4253b5dfc8f502ebc7b20be0facd
sxia1 <sxia1@stuy.edu>
2018-03-01 08:39:51 -0500

    update

---
+++ terminal_anim/go.kt
@@ -0,0 +1,28 @@
+//Sophia Xia
+//APCS2 pd1
+//HW12 -- +3R|\/|1|\|4|_  1|_|_|\|3$$
+//2018-03-01
+
+Q0: What do you make of the multiple class definitions in a single .java file?
+Notes: Boardasaurus is not public or private. If it were, the file would not compile. My hypothesis that this file would not run if there were 2 classes was only partially correct. Due to the lack of public in the declaration of class Boardasaurus I believe that Boardasaurus can only be accessed within Demo.java.
+
+
+Q1: How are the command line arguments processed?
+
+
+
+Q2: What happens if no command line arguments are given?
+
+
+
+Q3: What is the nature/purpose of the recursion? Is it necessary for the animation?
+
+
+
+Q4: What do you expect to see when you run it?
+
+
+
+Q5: What does ANSI stand for? (You are permitted some intertubes searching here…). What is it good for?
+
+
